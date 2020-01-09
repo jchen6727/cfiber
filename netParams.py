ff68ae5e4085fe5b325c15f32da7c208daeab4ac
jchen6727 <jchen.6727@gmail.com>
2020-01-09 10:30:05 -0500

    use markov models in cfiber_mkv.py and netParams.py

--- netParams.py
+++ netParams.py
@@ -8,7 +8,7 @@ except:
 netParams = specs.NetParams()   # object of class NetParams to store the network parameters
 netParams.popParams['cfiber'] = {'cellType': 'cfiber', 'numCells': 1, 'cellModel': '_cf'}
 #cfiber=netParams.importCellParams(label='cfiber', conds={'cellType': 'cfiber'}, fileName='cfiber.py', cellName='cfiber')
-cfiber=netParams.importCellParams(label='cfiber', conds={'cellType': 'cfiber'}, fileName='cfiber_.py', cellName='cfiber', cellArgs={'gnabar17': cfg.gnabar17, 'gnabar18': cfg.gnabar18, 'gnabar19': cfg.gnabar19})
+cfiber=netParams.importCellParams(label='cfiber', conds={'cellType': 'cfiber'}, fileName='cfiber_mkv.py', cellName='cfiber', cellArgs={'gnabar17': cfg.gnabar17, 'gnabar18': cfg.gnabar18, 'gnabar19': cfg.gnabar19})
 netParams.cellParams['cfiber'] = cfiber
 
 netParams.stimSourceParams['stim'] = {'type': 'IClamp', 'delay': 0, 'dur': 5, 'amp': 2}
